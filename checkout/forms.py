cec5ebdcbad3d1421843b5f66e644cb4a1650631
Giselle <gigi108@gmail.com>
2018-08-23 09:06:34 +0000

    strip setup started

--- checkout/forms.py
+++ checkout/forms.py
@@ -4,4 +4,4 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model=Order
-        fields = ('full_name', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address_1', 'street_address_2', 'county')
+        exclude = ('date',)
--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import OrderForm
+from .models import OrderLineItem
+from products.models import Product
+
 
 # Create your views here.
 def view_checkout(request):
-    return render(request, 'checkout/checkout.html')
+    if request.method == "POST":
+        form=OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            
+            cart=request.session.get('cart', {})
+            for product_id, quantity in cart.items():
+                lineItem=OrderLineItem(order=order, quantity=quantity, product_id = product_id)
+                lineItem.save()
+                
+            del request.session['cart']
+        return redirect("home")
+    else: 
+        form=OrderForm()
+    
+    cart = request.session.get('cart', {})
+    cart_items=[]
+    grand_total = 0
+    for product_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=product_id)
+        item_total = product.price * quantity
+        grand_total += item_total
+        cart_items.append({'product':product, 'quantity': quantity, 'item_total': item_total})
+        
+    return render(request, 'checkout/view_checkout.html', {'form': form, 'cart_items': cart_items, 'grand_total': grand_total})
