23e76f6e99c3a918b473a912dfebcb25d6dfbd40
Giselle <gigi108@gmail.com>
2018-08-24 13:42:54 +0000

    updated setting to run on Heroku with prod

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'cart',
     'checkout',
+    'storages',
 ]
 
 MIDDLEWARE = [
--- ecommerce/settings/prod.py
+++ ecommerce/settings/prod.py
@@ -1,14 +1,25 @@
 from .base import *
+import dj_database_url
 
 
-ALLOWED_HOSTS = ['#']
+ALLOWED_HOSTS = ['ringhioecommerce.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_S3_HOST = 's3.eu-west-2.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
